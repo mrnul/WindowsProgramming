fbb4fcdd83d4e15a328d5f9673c532d779150b0a
void90 <in.love.with.my.fairytale@gmail.com>
2015-12-14 15:29:51 +0200

    Update windowdefs.h

--- MyHeaders/windowdefs.h
+++ MyHeaders/windowdefs.h
@@ -4,13 +4,13 @@
 
 #include <Windows.h>
 
-#define CREATE_NEW_DATA_TYPE(name)	struct name																		\
-									{																				\
-										unsigned int val;															\
-										explicit name(unsigned int v) : val(v)			{ }							\
-										operator unsigned int() const					{ return val; }				\
-										name operator|(name v) const					{ return name(val | v); }	\
-										name operator|(unsigned int v) const			{ return name(val | v); }	\
-										~name() {}	/*is this ok?*/													\
-									}
-									
+#define CREATE_NEW_DATA_TYPE(name)	struct name									\
+					{																\
+						unsigned int val;							\
+						explicit name(unsigned int v) : val(v)	{ }				\
+						operator unsigned int() const		{ return val; }			\
+						name operator|(name v) const		{ return name(val | v); }	\
+						name operator|(unsigned int v) const	{ return name(val | v); }	\
+						~name() {} /*is this ok?*/						\
+					}
+	
